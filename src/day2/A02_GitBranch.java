1de1fda271c491dcc9df0dbabd293bb612492439
Ercan ALTUN <salsel0147@gmail.com>
2022-08-16 15:43:54 +0300

    V1

---
+++ src/day2/A02_GitBranch.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class A02_GitBranch {
+    public static void main(String[] args) {
+
+        /*
+         1) git branch [isim]  =========> yeni Branch olusturur
+         2) git branch  ================> Branch'teki mevcut durumu gosterir
+         3) git branch checkout [isim] => Branc'i aktif eder
+         4) git branch -m [isim] =======> Aktif olan Branc'in ismini degistirir
+         5) git branch -d [isim] =======> Aktif olmayan Branch'in adini siler(o anda aktif olmamali Branch)
+         */
+
+
+    }
+}
