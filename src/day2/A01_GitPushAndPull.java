5a47e614953116b636668f1b7b83463a393e3cec
Ercan ALTUN <salsel0147@gmail.com>
2022-08-16 10:22:44 +0300

    V2

---
+++ src/day2/A01_GitPushAndPull.java
@@ -0,0 +1,64 @@
+package day2;
+
+public class A01_GitPushAndPull {
+    public static void main(String[] args) {
+
+
+
+        /*
+        1-) git init ==>Local repo olusturmak icin yani . git ile
+        klasorunuzun icndeki dosyalari iliskilendirmek icin kullanilir
+        2-) git add . ==> Bunu Working Space 'den(yani yerel)dosyalarimi
+        Staginig Area'aya yani (Commit lemek icin beklenen yere gonderir)
+        3-) git commit -m"mesaj" ==> Stanging Area'dan commit store 'a dosyalar,mi
+        gondermek icin kullanilir(commit yani versiyon yani sürüm olusturmus olurum)
+        4-) git status == > Working Space 'deki ve Staging Arae'daki durumu gosterir
+
+        5-)git push ==> Uzak repo'ya yani(remote- GitHub)'a gondermek icin kullandigimiz kod
+        yalnız git push komutunu direk kullanmak istersek 1 kez
+                git remote add origin adress
+                git push -u origin master
+           Not: Yukaridaki 2 komutu tek seferde kullandıktan sonra diger 2. commit'lerim icin sadece
+                git push kullaniriz
+
+
+         */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
