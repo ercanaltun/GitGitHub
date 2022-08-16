756c4a1d1bd25fbae958a36fb6361e3dffa2c5f2
Ercan ALTUN <salsel0147@gmail.com>
2022-08-16 10:15:44 +0300

    V1

---
+++ src/day1/A1_GitVersion.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class A1_GitVersion {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git 1.Deneme");
+        System.out.println("Hello Git 2.Deneme");
+    }
+}
